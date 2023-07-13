4ffb23ae415146184f098dbbea12f849660b3f1b
bonbongiraffe <francescowai@yahoo.com>
2023-07-13 11:41:04 -0400

    first attempt

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 5, "M", "brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "yellow", "rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 22, "F", "lavender", "pensive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 24, "M", "brown", "disciplined", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "red", "well-spoken", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 50, "M", "green", "grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 17, "F", "blue", "relaxed", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, "M", "grey", "lost", 1);
